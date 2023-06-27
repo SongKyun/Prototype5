4d40c0247e817dcd777c6942d1d485df366b2bb3
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2023-06-27 23:42:13 +0900

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
+    private float spawnRate = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+        yield return new WaitForSeconds(spawnRate);
+        int index = Random.Range(0, targets.Count);
+        Instantiate(targets[index]);
+        }
+    }
+    
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score : " + score;
+    }
+}
